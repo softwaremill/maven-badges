0c9b3917d48ac590931bf72320e12c8cd8be1fbf
belfz <marcinbar1@gmail.com>
2018-07-09 17:10:44 +0200

    refresh expiry time on subsequent get requests to redis

--- src/services/shields.ts
+++ src/services/shields.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import RedisClientWrapper from './redisClientWrapper';
 
 const BASE_URI = 'http://img.shields.io';
+const TTL = 60 * 60 * 12;
 
 const encode = (input: string) => input.replace(/_/g, '__').replace(/\s/g, '_').replace(/-/g, '--');
 
@@ -10,13 +11,14 @@ export const getBadgeImage = async (redisClient: RedisClientWrapper, subject: st
   const serializedImageBuffer = await redisClient.getAsync(url);
   if (serializedImageBuffer) {
     console.log(`serving ${url} badge from cache`);
+    await redisClient.expireAsync(url, TTL); // refresh expiry time
     return new Buffer(serializedImageBuffer, 'hex');
   }
 
   const { data: buffer } = await axios.get(url, {
     responseType: 'arraybuffer'
   });
-  await redisClient.setAsync(url, buffer.toString('hex'), 'EX', 60 * 60 * 12); // sets an expiry time to 12h
+  await redisClient.setAsync(url, buffer.toString('hex'), 'EX', TTL); // sets an expiry time to 12h
   console.log(`saved ${url} badge to cache`);
   return buffer;
 }
