0af34361f9f84942e38e9e5edfe3fe190216e806
Lukasz Lenart <lukasz@softwaremill.com>
2024-10-17 09:30:37 +0200

    Fixes TLS in Redis

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import axios from 'axios';
 import { PORT, REDIS_URL } from './config';
 import { createServer } from './server';
 
-const redisClient: RedisClientType = createClient({url: REDIS_URL});
+const redisClient: RedisClientType = createClient({
+  url: REDIS_URL,
+  socket: {
+    tls: (REDIS_URL.match(/rediss:/) != null),
+    rejectUnauthorized: false,
+  }
+});
 
 redisClient.connect().then(() => {
   const app = createServer(axios, redisClient);
