008cb2d0ce2043724173ee0cd422a2d6d54b166e
Lukasz Lenart <lukasz@softwaremill.com>
2025-04-05 16:41:28 +0200

    Implement optional redirect if flag has been specified

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export class Config {
     this.redisPort = options.redisPort || process.env.PORT || '8080';
     this.redisUrl = options.redirectUrl || process.env.REDIS_URL || 'redis://127.0.0.1:6379';
     this.redirect = options.redirect || process.env.REDIRECT || 'false';
-    this.redirectUrl = options.redirectUrl || process.env.REDIRECT_URL || 'https://maven-badges.sml.io/';
+    this.redirectUrl = options.redirectUrl || process.env.REDIRECT_URL || 'https://maven-badges.sml.io';
   }
 
   isRedirect(): boolean {
