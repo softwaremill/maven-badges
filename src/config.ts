e4de87e4386cc418833ed75ab9aee6e9cf0ad47d
Lukasz Lenart <lukasz@softwaremill.com>
2025-04-05 16:41:01 +0200

    Introduces a config class to pass configuration

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,26 @@
-export const PORT = process.env.PORT || 8080;
-export const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+export interface ConfigOptions {
+  redisPort?: string;
+  redisUrl?: string;
+  redirect?: string;
+  redirectUrl?: string;
+}
+
+export class Config {
+  public redisPort: string;
+  public redisUrl: string;
+  public redirect: string;
+  public redirectUrl: string;
+
+  constructor(options: ConfigOptions = {}) {
+    this.redisPort = options.redisPort || process.env.PORT || '8080';
+    this.redisUrl = options.redirectUrl || process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+    this.redirect = options.redirect || process.env.REDIRECT || 'false';
+    this.redirectUrl = options.redirectUrl || process.env.REDIRECT_URL || 'https://maven-badges.sml.io/';
+  }
+
+  isRedirect(): boolean {
+    return this.redirect === 'true' && this.redirectUrl !== '';
+  }
+}
+
+export const config = new Config();
