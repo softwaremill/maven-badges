d56cee214ea7eccbd0522f9e0d816c9d07da563f
Lukasz Lenart <lukasz@softwaremill.com>
2025-04-06 08:40:52 +0200

    Adds loggers to redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { validateFormat } from './validation';
 import { NextFunction, Request, Response } from 'express';
 import { config } from "./config";
+import { Logger } from "heroku-logger";
+
+const logger = new Logger({prefix: 'middleware: '});
 
 export class HttpError extends Error {
   private statusCode: number;
@@ -22,7 +25,10 @@ export const validateFormatMiddleware = (req: Request, res: Response, next: Next
   validateFormat(req.params.format) ? next() : next(new HttpError(415, 'Invalid format'));
 
 export const optionalRedirect = (req: Request, res: Response, next: NextFunction) => {
-  config.isRedirect()
-    ? res.redirect(301, `${config.redirectUrl}${req.originalUrl}`)
-    : next()
+  if (config.isRedirect()) {
+    logger.info(`Redirecting traffic to: ${config.redirectUrl}`)
+    res.redirect(301, `${config.redirectUrl}${req.originalUrl}`)
+  } else {
+    next()
+  }
 };
