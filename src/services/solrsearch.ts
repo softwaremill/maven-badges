f7762265439093209321970803eaff57f8817157
Lukasz Lenart <lukasz.lenart@gmail.com>
2025-07-01 07:11:33 +0200

    Sorts version by timestamp (#1040)

--- src/services/solrsearch.ts
+++ src/services/solrsearch.ts
@@ -16,14 +16,23 @@ class NotFoundError extends Error {
 }
 
 export const getLastArtifactVersion = async (axios: AxiosStatic, groupId: string, artifact: string, useGav: boolean = false, repository: Repository = Repository.MAVEN_CENTRAL) => {
-  const url = `${repository}/solrsearch/select?q=g:${groupId}+AND+a:${artifact}&start=0&rows=1${useGav ? '&core=gav' : ''}`
+  const url = `${repository}/solrsearch/select?q=g:${groupId}+AND+a:${artifact}&start=0${useGav ? '&core=gav' : ''}`
   logger.info(`Requesting url ${url}`);
 
   const { data } = await axios.get<any>(url);
   const { response } = data;
-  if (response.numFound > 0) {
-    return response.docs[0].latestVersion || response.docs[0].v;
+  if (response.numFound > 1) {
+    logger.info(`Found last version of ${groupId}: ${response.numFound}`);
+    const sorted = response.docs.sort((v1: { timestamp: number }, v2: { timestamp: number }) => {
+      return v2.timestamp - v1.timestamp
+    })
+    return sorted[0].v;
   }
+  if (response.numFound == 1) {
+    logger.info(`Found last version of ${groupId}: ${response.numFound}`);
+    return response.docs[0].v;
+  }
+  logger.info(`Not found ${groupId}: ${response.numFound}`);
   throw new NotFoundError();
 };
 
